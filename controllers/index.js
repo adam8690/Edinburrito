4ce2bb19d9756edb816919fd7d3df24a5be0145f
Simon Smith <sds46@cantab.net>
2017-04-20 13:14:59 +0100

    server up and running

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ var express = require("express")
 var router = express.Router()
 var path = require("path")
 
-router.use("/api/burrito", require("./burritorouter.js"))
+router.use("/api/burrito", require("./burrito_router.js"))
 
 router.get("/", function (req, res) {
     // before you have index.html set up, you can just send this for testing:
