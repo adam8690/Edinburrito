b6fc29081dd7ffca9ad52af798ed05c94552e36f
Adam <adam8690@hotmail.co.uk>
2017-04-21 11:34:14 +0100

    map now zoom in on selected burrito places and highlights the selected place on in the listview

--- controllers/yelp.js
+++ controllers/yelp.js
@@ -6,6 +6,8 @@ var yelp = new Yelp({
   app_secret: 'vP3ZXssiL8cC15N2aaCFPeLcZanNjyh7Am9L2m0fkkcq956sX5jWZj8tWN196Znf'
 });
 
+
+
 // https://www.yelp.com/developers/documentation/v3/business_search
 
 
