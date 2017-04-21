d832ee4481a0da7ec6f462b2107b666c2eb8f174
Simon Smith <sds46@cantab.net>
2017-04-21 09:56:52 +0100

    display list of businesses

--- controllers/burrito_router.js
+++ controllers/burrito_router.js
@@ -3,13 +3,9 @@ var burritoRouter = express.Router()
 var yelp = require('./yelp')
 
 burritoRouter.get("/", function (req, res) {
-    yelpResults = yelp.search({term: 'burrito', location: 'edinburgh', limit: 10})
-    .then(function (data) {
-      res.json(JSON.parse(data).businesses);
-    })
-    
-    .catch(function (err) {console.error(err);});
-  
+    yelpResults = yelp.search({ term: 'burrito', location: 'edinburgh', limit: 20 })
+      .then(function (data) { res.send(data) })
+      .catch(function (err) { console.error(err) })
 })
 
 module.exports = burritoRouter
