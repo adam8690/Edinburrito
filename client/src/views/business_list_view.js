ceec0dcbaf8841703446295cdd28125f0bce1dea
Simon Smith <sds46@cantab.net>
2017-04-21 14:44:49 +0100

    rename highlight to select

--- client/src/views/business_list_view.js
+++ client/src/views/business_list_view.js
@@ -12,21 +12,14 @@ BusinessListView.prototype.render = function (businesses) {
     }
 
     businesses.forEach(function (business) {
-        // var coords = { lat: business.coordinates.latitude, lng: business.coordinates.longitude }
         var li = document.createElement("li")
         li.innerText = business.details.name + " " + business.details.rating + " " + business.details.price
         this.container.appendChild(li)
-        // var marker = this.mapWrapper.addMarker( coords )
-        // var infoWindow = this.addInfoWindow(marker, business)
         li.onclick = function(){
             this.highlight(li, business)  
-            // li.classList.add('selected')
             this.mapWrapper.googleMap.setCenter(business.coords)
             this.mapWrapper.googleMap.setZoom(16)
-            
         }.bind(this)
-      
-
     }.bind(this))
 
 }
