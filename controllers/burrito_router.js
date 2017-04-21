5a67781162d3b3b873d38767d4a1eb72ae8295fe
Adam <adam8690@hotmail.co.uk>
2017-04-21 16:36:56 +0100

    made api routes return json and added sample api creds object

--- controllers/burrito_router.js
+++ controllers/burrito_router.js
@@ -3,9 +3,15 @@ var burritoRouter = express.Router()
 var yelp = require('./yelp')
 
 burritoRouter.get("/", function (req, res) {
-    yelpResults = yelp.search({ term: 'burrito', location: 'edinburgh', limit: 20 })
-      .then(function (data) { res.send(data) })
+    var yelpResults = yelp.search({ term: 'burrito', location: 'edinburgh', limit: 20 })
+      .then(function (data) { res.json(JSON.parse(data)) })
       .catch(function (err) { console.error(err) })
+})
+
+burritoRouter.get('/business', function(req, res) {
+  var yelpResults = yelp.business('yuko-kitchen-los-angeles')
+.then(function (data) { res.json(JSON.parse(data)) })
+.catch(function (err) { res.json(err);});
 })
 
 module.exports = burritoRouter
--- controllers/yelp.js
+++ controllers/yelp.js
@@ -3,7 +3,11 @@ var apiCreds = require('../api_creds')
 
 var yelp = new Yelp(apiCreds);
 
-
+// api credentials are held in a separate gitignored file so they are not public. The object looks like this: 
+// var apiCreds = {
+//   app_id: '******',
+//   app_secret: '***************'
+// };
 
 // https://www.yelp.com/developers/documentation/v3/business_search
 
