479554de44853372cbd15964c5e4b350492154fd
Adam <adam8690@hotmail.co.uk>
2017-04-23 12:26:47 +0100

    added business details view

--- client/src/models/business.js
+++ client/src/models/business.js
@@ -2,28 +2,28 @@ var Business = function(details, mapWrapper){
   this.mapWrapper = mapWrapper
   this.coords = { lat: details.coordinates.latitude, lng: details.coordinates.longitude }
   this.details = details
+  this.moreDetails = null;
   this.marker = mapWrapper.addMarker(this.coords)
   this.infoWindow = mapWrapper.addInfoWindow(details.name, this.marker)
-
   this.marker.addListener("click", function() {
-    this.mapWrapper.openInfoWindow(this)
+  this.mapWrapper.openInfoWindow(this)
   }.bind(this))
 
 }
 
 Business.prototype.openInfoWindow = function () {
   this.infoWindow.open(this.mapWrapper, this.marker)
+  this.getMoreDetails()
 }
 
-Business.prototype.getDetails = function () {
+Business.prototype.getMoreDetails = function () {
     var request = new XMLHttpRequest();
     request.open("GET", "http://localhost:3000/api/businesses/"+ this.details.id);
     request.onload = function(){
         if ( request.status !== 200 ) return 
             var jsonString = request.responseText
             var businessDetails = JSON.parse(jsonString)
-            console.log('Business Details: ', businessDetails)
-            return businessDetails;
+            this.moreDetails = businessDetails;
     }
     request.send()
 }
---
+++ client/src/views/business_detail_view.js
@@ -0,0 +1,8 @@
+var BusinessDetailView = function(business){
+console.log(business.details);
+console.log(business.moreDetails)
+}
+
+
+
+module.exports = BusinessDetailView
