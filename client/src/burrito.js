bc1fc49491210a6dd0d434828ecc46b967ee78fc
Adam <adam8690@hotmail.co.uk>
2017-04-25 15:07:52 +0100

    got a new webpack bundle working and added a new page and js file to handle the burrito info page.

---
+++ client/src/burrito.js
@@ -0,0 +1,14 @@
+
+var initialize = function(){
+  console.log('burrito js lives!!')
+
+  var body = document.querySelector("body")
+  var flexGrid = document.querySelector(".flex-grid")
+
+
+}
+
+
+
+
+window.onload = initialize
