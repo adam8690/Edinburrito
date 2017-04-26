29edacc947abe31cebf18692a397024116048726
Adam <adam8690@hotmail.co.uk>
2017-04-26 14:40:58 +0100

    css improvments

--- client/src/views/what_is_burrito.js
+++ client/src/views/what_is_burrito.js
@@ -12,10 +12,9 @@ WhatIsView.prototype.render = function(){
   div.appendChild(h2);
 
   var descriptionP = document.createElement('p');
-  var br = document.createElement('br');
-  descriptionP.innerText = "A burrito is a type of Mexican and Tex-Mex food, consisting of a large wheat flour tortilla with a filling, wrapped into a closed-ended cylinder, in contrast to a taco, where the tortilla is simply folded around the filling. The flour tortilla is sometimes lightly grilled or steamed to soften it, make it more pliable and allow it to adhere to itself when wrapped. In Mexico, meat and refried beans are sometimes the only fillings. In the United States, burrito fillings may include a combination of ingredients such as Mexican-style rice or plain rice, beans or refried beans, lettuce, salsa, meat, guacamole, cheese, sour cream and various vegetables. Burrito size varies."
 
-  div.appendChild(br);
+  descriptionP.innerHTML = "<br>A burrito is a type of Mexican and Tex-Mex food, consisting of a large wheat flour tortilla with a filling, wrapped into a closed-ended cylinder, in contrast to a taco, where the tortilla is simply folded around the filling. The flour tortilla is sometimes lightly grilled or steamed to soften it, make it more pliable and allow it to adhere to itself when wrapped. In Mexico, meat and refried beans are sometimes the only fillings. In the United States, burrito fillings may include a combination of ingredients such as Mexican-style rice or plain rice, beans or refried beans, lettuce, salsa, meat, guacamole, cheese, sour cream and various vegetables. Burrito size varies."
+
   div.appendChild(descriptionP);
 
 
